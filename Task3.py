b2a291988bd0430d07af79d1f980926e7cc063bb
Kit1001 <eitelkeit999@gmail.com>
2021-11-15 19:56:06 +0300

    seventh lessson

---
+++ Task3.py
@@ -0,0 +1,29 @@
+# Не успел доделать до конца, в алгоритме где-то ошибка, работает не в 100% случаев.
+# Скрипт берет случайное число из списка и формирует два списка, в одном все числа больше, в другом меньше случайного.
+# Далее сравнивается размер двух списков и процедура повторяется для большего списка
+# пока не получится два одинаковых по размеру списка, с медианой в середине.
+
+import random
+
+
+def find_median(list_, n=0):
+    if len(list_) == 1:
+        return list_[0]
+    random_index = random.randint(0, len(list_) - 1)
+    list_a = [x for x in list_ if x < list_[random_index]]
+    list_b = [x for x in list_ if x > list_[random_index]]
+    if len(list_a) + n == len(list_b):
+        return list_[random_index]
+    elif len(list_a) + n > len(list_b):
+        n -= len(list_b)
+        return find_median(list_a, n)
+    elif len(list_b) > len(list_a) + n:
+        n += len(list_a)
+        return find_median(list_b, n)
+
+
+random.seed(42)
+some_list = list(range(9))  # 5. Ломается на числе 13
+random.shuffle(some_list)
+print(some_list)
+print(find_median(some_list))
