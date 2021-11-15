f3e5d0ecea6d8401d031bb8892e3c7d7effe812f
Kit1001 <92787705+Kit1001@users.noreply.github.com>
2021-11-15 20:50:51 +0300

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,15 +1,12 @@
 # Скрипт берет случайное число из списка и формирует два списка, в одном все числа больше, в другом меньше случайного.
 # Далее сравнивается размер двух списков и процедура повторяется для большего списка
 # пока не получится два одинаковых по размеру списка, с медианой в середине.
-
 import random
 
 
 def find_median(list_, n=0):
-    if len(list_) == 1:
-        return list_[0]
     random_index = random.randint(0, len(list_) - 1)
-    list_a = [x for x in list_ if x < list_[random_index]]
+    list_a = [x for x in list_ if x <= list_[random_index]]
     list_b = [x for x in list_ if x > list_[random_index]]
     if len(list_a) + n == len(list_b):
         return list_[random_index]
@@ -22,7 +19,6 @@ def find_median(list_, n=0):
 
 
 random.seed(42)
-some_list = [random.randint(-100, 100) for _ in range(random.randint(10, 100) * 2 + 1)]
-print(some_list)
-print(find_median(some_list))  # получается -3
-print(sorted(some_list)[len(some_list) // 2 + 1])  # проверяем результат с помощью сортировки, ответ тот же
+some_list = [random.randint(-100, 100) for _ in range(random.randint(5, 5000) * 2 + 1)]
+print('median result', find_median(some_list))  # получается 3
+print('sorted result', sorted(some_list)[len(some_list) // 2])  # проверяем результат с помощью сортировки
